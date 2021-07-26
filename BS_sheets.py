b22427100f7908eb89704111901051fd382f4b5a
MicroTot <32261713+MicroTot@users.noreply.github.com>
2021-07-26 14:58:06 +0300

    Create BS_sheets.py

---
+++ BS_sheets.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+def scraped():
+    scraps = []
+    url = "https://www.google.com/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    clean = soup.prettify()
+    print(clean)
+    # print(rows)
+    rows = soup.find_all(class_= "")
+    for r in rows:
+        item1=r.find('h1').text
+        item2=r.find('h2').text
+        item3=r.find('h3').text
+        item4=r.find('h4').text
+        # print(item1, item2, item3, item4)
+        scrap={
+            'item1': item1,
+            'item2': item2,
+            'item3': item3,
+            'item4': item4,
+        }
+        scraps.append(scrap)
+    header = ['ITEM1', 'ITEM2', 'ITEM3', 'ITEM4', 'ITEM5']
+    data = [item1, item2, item3, item4]
+    with open('name.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerow(data)
+scraped()
